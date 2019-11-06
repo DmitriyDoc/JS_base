d2c33f30ec834fd637a3fa3231e449299ed2abb4
DmitriyD <f.doc2@yandex.ru>
2019-11-06 14:01:18 +0300

    Типы данных

---
+++ app/js/custom.js
@@ -0,0 +1,22 @@
+'use strict'
+
+//Типы данных
+let myNumber = 2525, // number
+    myString = "Some string", //string
+    myBool = true, // boolean
+    myNull = null, // object (известная ошибка)
+    myUndef = undefined; // undefined
+
+//Объектные типы
+
+let obj = {name: "string"},
+    array = [1,2,3],
+    regexp = /w+/g,
+    func = function(){};
+
+console.log(typeof(obj));  
+console.log(typeof(array));
+console.log(typeof(regexp)); 
+console.log(typeof(func));
+
+
