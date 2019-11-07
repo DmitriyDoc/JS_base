61d064b2a6d3b5c5e18b4d6d41e1b34941513af0
DmitriyD <f.doc2@yandex.ru>
2019-11-07 20:59:04 +0300

    Циклы

---
+++ app/js/custom2.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//Циклы
+let it, itr, iw, id;
+
+//For
+for (it = 0; it < 10; it++) {
+    console.log(it);
+} 
+
+for (itr = 0; itr < 10; itr++) {
+    console.log(itr);
+}
+
+//While
+iw = 0;
+while (iw < 10) {
+    console.log(iw);
+        iw++
+}
+
+// Do Whiile
+id = 0;
+do console.log(id++); while (id < 10)
